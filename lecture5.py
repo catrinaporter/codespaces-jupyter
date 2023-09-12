7c29ee75d2fd91fb038d1c9a6714b1bd1a9d18aa
catrinaporter <143296916+catrinaporter@users.noreply.github.com>
2023-09-12 16:48:03 +0000

    lecture5

---
+++ lecture5.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+foo = ["a", "b"]
+foo2 = ["b", "a"]
+foo == foo2
+foo2.sort()
+foo2
+foo == foo2
+animals = {'dog', 'cat'}
+type(animals)
+animals2 = {'cat', 'dog'}
+animals == animals2
+animals.add('turtle')
+animals2.add('fish'))
+animals2.add('fish')
+animals.union(animals2)
+animals.intersection(animals2)
+animals3 = {'cow'}
+animals2.intersection(animals3)
+set(['a', 'b'])
+list(set(['a', 'b']))
+animal_legs = {'dog':4, 'ant':6}
+animal_legs['dog']
+animal_legs['ant']
+animal_legs['spider'] = 8
+animal_legs['spider']
+animal_legs.keys()
+animal_legs.values()
+animal
+animals
+legs = {4,4,4}
+animal_legs_data = [('cat', 4), ('dog', 4), ('turtle', 4)]
+print(animal_legs_data)
+zip(animals, legs)
+list(zip(animals, legs))
+for thing in animal_legs:
+    print(thing)
+    
+for key, value in animal_legs.items():
+    print("animal: ", key, "legs: ", value)
+    
+data2 = {'animals': ['dog', 'cat', 'bob'], 'legs:[4,4,2]}
+data2 = {'animals': ['dog', 'cat', 'bob'], 'legs':[4,4,2]}
+data2
